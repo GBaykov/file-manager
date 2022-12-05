456a214e20a102a75b9707f0a2691933a7469665
GBaykov <gleb.baykov99@gmail.com>
2022-12-05 23:18:49 +0300

    feat: implement "delete" func

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { stat } from "node:fs/promises";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  if (fs.existsSync(path.join(__dirname, "files", "fileToRemove.txt"))) {
+    fs.unlink(
+      path.join(__dirname, "files", "fileToRemove.txt"),
+      function (err) {
+        if (err) throw new Error("FS operation failed");
+      }
+    );
+  } else throw new Error("FS operation failed");
 };
 
 await remove();
