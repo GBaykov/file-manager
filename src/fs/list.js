b798cbb34d394362cebf8063ad1f304535ff12f6
GBaykov <gleb.baykov99@gmail.com>
2022-12-05 23:32:04 +0300

    feat: implement "list"

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import { readdir } from "node:fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const { stdout } = process;
+
 const list = async () => {
-    // Write your code here 
+  fs.access(path.join(__dirname, "files"), async function (err) {
+    if (err && err.code === "ENOENT") throw new Error("FS operation failed");
+
+    const files = await readdir(path.join(__dirname, "files"));
+    for (const file of files) {
+      //   stdout.write(file);
+      console.log(file);
+    }
+  });
 };
 
 await list();
