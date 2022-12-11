d0022ad002d547fe842df3d6246d835a91e0f517
GBaykov <gleb.baykov99@gmail.com>
2022-12-11 20:49:14 +0300

    feat: implement transorm file

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { stdin, stdout } from "process";
+import { pipeline, Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const myTransform = new Transform({
+    transform(chunk, encoding, cb) {
+      cb(null, chunk.toString().split("").reverse().join("") + "\n");
+    },
+  });
+  pipeline(stdin, myTransform, stdout, (err) => err);
 };
 
 await transform();
